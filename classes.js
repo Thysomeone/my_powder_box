2db3b499d99cca7c8b5429d2e2c111be785b899f
Thysomeone <thevladlupulescu@gmail.com>
2026-08-06 22:51:08 +0300

    added custom physics
    added water
    added
    max_horizontal_slide
    max_vertical_diffrence

--- classes.js
+++ classes.js
@@ -16,7 +16,7 @@ class pixel
         this.can_fall=can_fall;
         this.can_move=can_move;
         this.max_horizontal_slide=max_horizontal_slide;
-        this.max_vertical_diffrence=max_vertical_diffrence; //how many blocks can be on top of eatchother until they fall
+        this.max_vertical_diffrence=max_vertical_diffrence; //minimum 1, you can make a tower of x-1 blocks
     }
 }
 
@@ -24,13 +24,13 @@ class pixel
 //heavier pixels fall trough lighter ones
 
 //pixel variables
-let air = new pixel(0,"#8F8F8F",0,true,true,5,5)
+let air = new pixel(0,"#8F8F8F",0,true,true,1,50)
 
 let solid_barrier = new pixel(1,"#303030",100,false,false,5,5)
 
-let sand = new pixel(2,"#ccc205",10,true,true,3,3)
+let sand = new pixel(2,"#ccc205",10,true,true,2,3)
 
-let water = new pixel(3,"#055fcc",10,true,true,5,1)
+let water = new pixel(3,"#055fcc",5,true,true,5,1)
 //pixel list
 const pixels =[air,solid_barrier,sand,water];
 const added_pixel_amount = 3;
--- functions.js
+++ functions.js
@@ -64,44 +64,124 @@ function pixel_fall(id_map,update_map,p_y,p_x)
     let pixel = pixels[id_map[p_y][p_x]];
     let max_horizontal_slide=pixel.max_horizontal_slide;
     let max_vertical_diffrence=pixel.max_vertical_diffrence;
-    if(pixel.can_fall==true)
+
+    if(pixel.can_fall==true && p_y+1<max_y) //simple fall down logic
     {
-        if(p_y+1<max_y)
+        if(pixels[id_map[p_y+1][p_x]].weight<pixel.weight && pixels[id_map[p_y+1][p_x]].can_move)
         {
-            if(pixels[id_map[p_y+1][p_x]].weight < pixel.weight )//&& pixel_map[p_y+1][p_x].can_move==true)
-            {
-                update_map[p_y][p_x]=true;
-                id_map[p_y][p_x]=id_map[p_y+1][p_x];
-                update_map[p_y+1][p_x]=true;
-                id_map[p_y+1][p_x]=pixel.id;
-                return;
-            }
+            update_map[p_y][p_x]=true;
+            id_map[p_y][p_x]=id_map[p_y+1][p_x];
+            update_map[p_y+1][p_x]=true;
+            id_map[p_y+1][p_x]=pixel.id;
+            return;
         }
 
-        if(p_y+1<max_y && p_x+1<max_x)
-        {
-            if(pixels[id_map[p_y+1][p_x+1]].weight<pixel.weight )//&& pixel_map[p_y+1][p_x+1].can_move==true)
-            {
-                update_map[p_y][p_x]=true;
-                id_map[p_y][p_x]=id_map[p_y+1][p_x+1];
-                update_map[p_y+1][p_x+1]=true;
-                id_map[p_y+1][p_x+1]=pixel.id;
-                return;
-            }
-        }
+    }
 
-        if(p_y+1<max_y && p_x-1>=0)
-        {
-            if(pixels[id_map[p_y+1][p_x-1]].weight<pixel.weight )//&& pixel_map[p_y+1][p_x-1].can_move==true)
-            {
-                id_map[p_y][p_x]=id_map[p_y+1][p_x-1];
-                update_map[p_y][p_x]=true;
-                update_map[p_y+1][p_x-1]=true;
-                id_map[p_y+1][p_x-1]=pixel.id;
-                return;
-            }
-        }
 
+    if(pixel.can_fall==true && p_y+max_vertical_diffrence<max_y && p_y+1<max_y)
+    {
+
+        //we check the possible end positions
+        let can_go_left=true;
+        let can_go_right=true;
+        for(let i=1;i<=max_horizontal_slide;i++)
+        {
+            if(p_x-i>=0 && can_go_left)//check for the right side
+            {
+                let valid = true;
+                if(pixels[id_map[p_y][p_x-i]].weight<pixel.weight && pixels[id_map[p_y][p_x-i]].can_move)
+                {
+
+                    for(let j=p_y+max_vertical_diffrence; j>p_y;j--)
+                    {
+                        if(pixels[id_map[j][p_x-i]].weight<pixel.weight && pixels[id_map[j][p_x-i]].can_move)
+                        {
+
+                        }
+                        else
+                        {
+                            valid=false;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    can_go_left=false;
+                    valid=false;
+                }
+
+                if(valid)
+                {
+                    if(i==1)
+                    {
+                    update_map[p_y][p_x]=true;
+                    id_map[p_y][p_x]=id_map[p_y+1][p_x-1];
+                    update_map[p_y+1][p_x-1]=true;
+                    id_map[p_y+1][p_x-1]=pixel.id;
+                    return;
+                    }
+                    else
+                    {
+                    update_map[p_y][p_x]=true;
+                    id_map[p_y][p_x]=id_map[p_y][p_x-1];
+                    update_map[p_y][p_x-1]=true;
+                    id_map[p_y][p_x-1]=pixel.id;
+                    return;
+                    }
+                }
+            }
+
+            if(p_x+i<max_x && can_go_right)//check for the left side
+            {
+                let valid = true;
+                if(pixels[id_map[p_y][p_x+i]].weight<pixel.weight && pixels[id_map[p_y][p_x+i]].can_move)
+                {
+
+                    for(let j=p_y+max_vertical_diffrence; j>p_y;j--)
+                    {
+                        if(pixels[id_map[j][p_x+i]].weight<pixel.weight && pixels[id_map[j][p_x+i]].can_move)
+                        {
+
+                        }
+                        else
+                        {
+                            valid=false;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    can_go_right=false;
+                    valid=false;
+                }
+
+                if(valid)
+                {
+                    if(i==1)
+                    {
+                    update_map[p_y][p_x]=true;
+                    id_map[p_y][p_x]=id_map[p_y+1][p_x+1];
+                    update_map[p_y+1][p_x+1]=true;
+                    id_map[p_y+1][p_x+1]=pixel.id;
+                    return;
+                    }
+                    else
+                    {
+                    update_map[p_y][p_x]=true;
+                    id_map[p_y][p_x]=id_map[p_y][p_x+1];
+                    update_map[p_y][p_x+1]=true;
+                    id_map[p_y][p_x+1]=pixel.id;
+                    return;
+                    }
+                }
+            }
+
+            if(can_go_left==false && can_go_right==false)
+                break;
+        }
     }
 
 }
